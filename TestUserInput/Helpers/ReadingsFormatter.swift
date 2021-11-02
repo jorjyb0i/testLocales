f29932b4ba32a9855e6fc78db51db75df518a703
jorjyb0i <pof717@gmail.com>
2021-11-02 15:57:16 +0300

    no message

--- TestUserInput/Helpers/ReadingsFormatter.swift
+++ TestUserInput/Helpers/ReadingsFormatter.swift
@@ -8,30 +8,37 @@
 import UIKit
 
 class ReadingsFormatter {
-    static let local: NumberFormatter = {
-        let formatter = NumberFormatter()
-        formatter.numberStyle = .decimal
-        formatter.maximumFractionDigits = 3
-        formatter.locale = .current
-        formatter.roundingMode = .floor
-        return formatter
-    }()
     
-    static let server: NumberFormatter = {
-        let formatter = NumberFormatter()
-        formatter.numberStyle = .decimal
-        formatter.maximumFractionDigits = 3
-        formatter.groupingSeparator = ""
-        formatter.decimalSeparator = "."
-        formatter.roundingMode = .floor
-        return formatter
-    }()
+    static let `default` = ReadingsFormatter()
     
-    static let allowedCharacters: CharacterSet = {
+    init() {
+        let localFormatter = NumberFormatter()
+        localFormatter.numberStyle = .decimal
+        localFormatter.maximumFractionDigits = 3
+        localFormatter.locale = .current
+        localFormatter.roundingMode = .floor
+        
+        self.local = localFormatter
+        
+        let serverFormatter = NumberFormatter()
+        serverFormatter.numberStyle = .decimal
+        serverFormatter.maximumFractionDigits = 3
+        serverFormatter.groupingSeparator = defaultGroupingSeparator
+        serverFormatter.decimalSeparator = defaultDecimalSeparator
+        serverFormatter.roundingMode = .floor
+        
+        self.server = serverFormatter
+        
         let set = "0123456789" + (local.decimalSeparator ?? "") + (local.groupingSeparator ?? "")
-        return CharacterSet(charactersIn: set)
-    }()
+        
+        self.allowedCharacters = CharacterSet(charactersIn: set)
+    }
     
-    static let defaultGroupingSeparator = ""
-    static let defaultDecimalSeparator = "."
+    var local: NumberFormatter
+    var server: NumberFormatter
+    
+    var allowedCharacters: CharacterSet
+    
+    var defaultGroupingSeparator = ""
+    var defaultDecimalSeparator = "."
 }
--- TestUserInput/MainView/ViewController.swift
+++ TestUserInput/MainView/ViewController.swift
@@ -27,106 +27,128 @@ class ViewController: UIViewController {
         view.backgroundColor = .lightGray
         
         content.textField.delegate = self
-        content.textField.addTarget(self, action: #selector(manageInputFormat), for: .editingChanged)
         content.button.addTarget(self, action: #selector(didPressOnButton), for: .touchUpInside)
         
-        let number = ReadingsFormatter.server.number(from: self.serverData) ?? 0
-        content.label.text = ReadingsFormatter.local.string(from: number)
+        let number = formatterManager.server.number(from: self.serverData) ?? 0
+        content.label.text = formatterManager.local.string(from: number)
     }
     
     @objc func didPressOnButton() {
         let text = content.textField.text ?? "nil"
-        let number = ReadingsFormatter.local.number(from: text) ?? -1
-        let numberToSend = ReadingsFormatter.server.string(from: number) ?? "nil"
+        let number = formatterManager.local.number(from: text) ?? -1
+        let numberToSend = formatterManager.server.string(from: number) ?? "nil"
         content.buttonLabel.text = numberToSend
     }
     
     //MARK: -- Formatter Section
     var lastEnteredText: String = ""
+    let formatterManager = ReadingsFormatter.default
     
-    @objc func manageInputFormat() {
-        guard
-            let userInput = content.textField.text,
-            ReadingsFormatter.allowedCharacters.isSuperset(of: CharacterSet(charactersIn: userInput)),
-            userInput.components(separatedBy: ReadingsFormatter.local.decimalSeparator).count < 3,
-            userInput.numberInStringHasLessThanOrEqualTo(numberOfFractionalDigits: ReadingsFormatter.local.maximumFractionDigits)
-        else {
-            content.textField.text = lastEnteredText
-            setPosition(isInputValid: false)
-            return
-        }
-        
-        let formattedInput = userInput.formatUserInput()
-        
-        guard lastEnteredText != formattedInput else { return }
-        
-        lastEnteredText = formattedInput
-        content.textField.text = formattedInput
-        setPosition(isInputValid: true)
+    private func isInputValid(input: String) -> Bool {
+        let isAllowedCharacters = formatterManager.allowedCharacters.isSuperset(of: CharacterSet(charactersIn: input))
+        let onlyOneDecimalSeparator = input.components(separatedBy: formatterManager.local.decimalSeparator).count < 3
+        let hasAllowedAmountOfDecimalDigits = decimalPartIsNotLonger(than: formatterManager.local.maximumFractionDigits, in: input)
+        return isAllowedCharacters && onlyOneDecimalSeparator && hasAllowedAmountOfDecimalDigits
     }
     
-    //MARK: -- Cursor Section
-    enum TextFieldChangeType {
-        case insert
-        case deleteOne
-        case deleteSelection
+    private func decimalPartIsNotLonger(than numberOfDigits: Int, in input: String) -> Bool {
+        guard input.contains(formatterManager.local.decimalSeparator) else { return true }
+        let result = inputWithDefaultSeparators(input: input).components(separatedBy: formatterManager.local.decimalSeparator).last ?? ""
+        return result.count <= numberOfDigits
     }
     
-    var changeType: TextFieldChangeType = .deleteOne
-    var lastPosition: Int = 0
-    var lengthOfInput: Int = 0
-    var previousInput: String = ""
+    private func inputWithDefaultSeparators(input: String) -> String {
+        let result = input
+            .replacingOccurrences(of: formatterManager.local.groupingSeparator ?? "", with: formatterManager.defaultGroupingSeparator)
+            .replacingOccurrences(of: formatterManager.local.decimalSeparator ?? "", with: formatterManager.defaultDecimalSeparator)
+        return result
+    }
     
-    func setPosition(isInputValid: Bool) {
-        let textField = content.textField
+    private func isDeletingGroupingSeparator(input: String) -> Bool {
+        guard let separator = formatterManager.local.groupingSeparator else { return false }
+        let isDeletingSingleSymbol = (lastEnteredText.count - input.count) == 1
+        let isDeletingGroupingSeparator = (lastEnteredText.components(separatedBy: separator).count - input.components(separatedBy: separator).count) == 1
+        return isDeletingSingleSymbol && isDeletingGroupingSeparator
+    }
+    
+    private func format(userInput: String) -> String {
+        guard !userInput.isEmpty else { return "" }
+        let inputWithDefaultSeparators = inputWithDefaultSeparators(input: userInput)
+        let numberToFormat = formatterManager.server.number(from: inputWithDefaultSeparators) ?? 0
+        let formattedInput = formatterManager.local.string(from: numberToFormat)
         
-        guard isInputValid else {
-            if let samePosition = textField.position(from: textField.beginningOfDocument, offset: lastPosition) {
-                textField.selectedTextRange = textField.textRange(from: samePosition, to: samePosition)
-            }
-            return
+        guard var resultString = formattedInput else { return "" }
+        
+        if userInput.contains(formatterManager.local.decimalSeparator) {
+            guard
+                let separator = formatterManager.local.decimalSeparator,
+                let integerPart = resultString.components(separatedBy: separator).first
+            else { return "" }
+            
+            let components = userInput.components(separatedBy: separator)
+            guard components.count == 2, let decimalPart = components.last else { return "\(integerPart)\(separator)"}
+            
+            resultString = "\(integerPart)\(separator)\(decimalPart)"
         }
         
-        var offset: Int = 0
-        
-        guard let currentInput = textField.text else { return }
-        
-        switch changeType {
-        case .insert:
-            offset = (lastPosition + lengthOfInput).includingSeparators(previousInput: previousInput, currentInput: currentInput)
-        case .deleteOne:
-            offset = (lastPosition - 1).includingSeparators(previousInput: previousInput, currentInput: currentInput)
-        case .deleteSelection:
-            offset = (lastPosition - lengthOfInput).includingSeparators(previousInput: previousInput, currentInput: currentInput)
-        }
-        
-        if let newPosition = textField.position(from: textField.beginningOfDocument, offset: offset) {
-            textField.selectedTextRange = textField.textRange(from: newPosition, to: newPosition)
-        }
+        return resultString
+    }
+    
+    //MARK: -- Cursor section
+    private func setPosition(of textField: UITextField, offset: Int) {
+        guard let newPosition = textField.position(from: textField.beginningOfDocument, offset: offset) else { return }
+        textField.selectedTextRange = textField.textRange(from: newPosition, to: newPosition)
     }
 }
 
 extension ViewController: UITextFieldDelegate {
+    
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-
-        if let selectedRange = textField.selectedTextRange {
-            let cursorPosition = textField.offset(from: textField.beginningOfDocument, to: selectedRange.start)
-            
-            lastPosition = cursorPosition
-            lengthOfInput = string.count
-            
-            if string.count > 0 {
-                changeType = .insert
-            } else if range.length == 1 {
-                changeType = .deleteOne
-            } else {
-                changeType = .deleteSelection
+        let hasComeFromUserInput = !(range.length == 0 && string.isEmpty)
+        guard
+            hasComeFromUserInput,
+            let userInput = (textField.text as NSString?)?.replacingCharacters(in: range, with: string),
+            isInputValid(input: userInput)
+        else { return false }
+        
+        if isDeletingGroupingSeparator(input: userInput) {
+            if let selectedRange = textField.selectedTextRange {
+                let cursorPosition = textField.offset(from: textField.beginningOfDocument, to: selectedRange.start)
+                if let newPosition = textField.position(from: textField.beginningOfDocument, offset: cursorPosition - 1) {
+                    textField.selectedTextRange = textField.textRange(from: newPosition, to: newPosition)
+                }
+                return false
             }
-            
-            previousInput = textField.text ?? ""
         }
         
-        return true
+        let formattedInput = format(userInput: userInput)
+        var offset = formattedInput.count - (lastEnteredText.count - (range.location + range.length))
+        offset = offset >= 0 ? offset : 0
+        
+        guard lastEnteredText != formattedInput else {
+            setPosition(of: textField, offset: offset)
+            return false
+        }
+    
+        lastEnteredText = formattedInput
+        textField.text = formattedInput
+        
+        setPosition(of: textField, offset: offset)
+        return false
+    }
+}
+
+class StrangeTextField: UITextField {
+    override var selectedTextRange: UITextRange? {
+        set {
+            super.selectedTextRange = newValue
+            if let range = newValue {
+                print("SelectedRange: \n  Start: \(range.start)\n  End:   \(range.end)\n")
+            }
+        }
+        get {
+            return super.selectedTextRange
+        }
     }
 }
 
